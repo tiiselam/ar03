7ca8bfd61ee662a8eb800e4fa3dc2fbfe0131a13
linatoro156 <linatoro156@hotmail.com>
2018-09-19 17:54:55 -0300

    Dll

---
+++ ARG_GP2016/FACTURA_ELECTRONICA/app/FEEXP/FEEXP/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("FEEXP")> 
+<Assembly: AssemblyDescription("Facturación Electronica Exportación")> 
+<Assembly: AssemblyCompany("Tiiselam")> 
+<Assembly: AssemblyProduct("FEEXP")> 
+<Assembly: AssemblyCopyright("Copyright ©  2018")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(True)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("e4a11d02-4ee6-49cd-bb02-985c2aba2ab7")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
